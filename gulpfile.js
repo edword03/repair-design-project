1d614b06f53432b22c594c6e90f8624dac4e52e1
edword03 <dima_veretennikov@list.ru>
2020-03-03 20:31:28 +0300

    feat: add header in index.html

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
+const cssmin = require('gulp-cssmin');
+const rename = require('gulp-rename');
 
 gulp.task('hello', function (done) {
   console.log('Hello world');
@@ -14,4 +16,11 @@ gulp.task('browser-sync', function() {
         }
     });
     gulp.watch("./*.html").on('change', browserSync.reload);
+});
+
+gulp.task('default', function () {
+  gulp.src('css/*.css')
+      .pipe(cssmin())
+      .pipe(rename({suffix: '.min'}))
+      .pipe(gulp.dest('dist'));
 });
