ba1ea9076d754b928816f397580081380652ea71
edword03 <dima_veretennikov@list.ru>
2020-03-20 21:14:44 +0300

    feat: added JQuery and plagins for him, add section control, footer, project. Added video-phone, added form validation, added slider, added responsive for site

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function(event) { 
+/*document.addEventListener("DOMContentLoaded", function(event) { 
   const modal = document.querySelector('.modal');
   const modalBtn = document.querySelectorAll('[data-toggle=modal]');
   const closeBtn = document.querySelector('.modal__close');
@@ -11,5 +11,134 @@ document.addEventListener("DOMContentLoaded", function(event) {
   });
 
   closeBtn.addEventListener('click', switchModal);
+
+});
+*/
+
+
+$(document).ready(function () {
+  var modal = $('.modal'),
+      modalBtn = $('[data-toggle="modal"]'),
+      closeBtn = $('.modal__close');
+
+  modalBtn.on('click', function() {
+    modal.toggleClass('modal--visible');
+  });
+  closeBtn.on('click', function(){
+    modal.toggleClass('modal--visible');
+  });
+
+  var mySwiper = new Swiper ('.swiper-container', {
+    // Optional parameters
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  })
+
+
+  var next = $('.swiper-button-next');
+  var prev = $('.swiper-button-prev');
+  var bullets = $('.swiper-pagination');
+
+  next.css('left', prev.width() + 10 + bullets.width() +10 )
+  bullets.css('left', prev.width() + 10)
+
+
+  new WOW().init();
+
+//валидация формы
+
+//валидация всплывающего окна формы
+$('.modal__form').validate({
+  errorClass: "invalid",
+  rules: {
+    // simple rule, converted to {required:true}
+    userName: {
+      required: true,
+      minlength: 2
+    },
+    userPhone: {
+      required: true,
+      minlength: 17
+    },
+    // compound rule
+    userEmail: {
+      required: true,
+      email: true
+    }
+  }, // сообщение 
+  messages: {
+    userName: {
+      required: "Имя обязательно к заполнению",
+      minlength: "Имя должно быть не короче двух букв"
+    },
+    userPhone: "Телефон обязателен",
+    userEmail: {
+      required: "Обязательно укажите email",
+      email: "Введите в формате name@domain.com"
+    }
+  }
+
+});
+
+//Валидация формы подвала
+$('.footer__form').validate({
+  errorClass: "invalid",
+  rules: {
+    // simple rule, converted to {required:true}
+    userName: {
+      required: true,
+      minlength: 2
+    },
+    userPhone: {
+      required: true,
+      minlength: 17
+    },
+    userQuestion: "required",
+  }, // сообщение 
+  messages: {
+    userName: {
+      required: "Имя обязательно к заполнению",
+      minlength: "Имя должно быть не короче двух букв"
+    },
+    userPhone: "Телефон обязателен",
+    userQuestion: "Заполните пожалуйста поле"
+  }
+
+});
+
+//Валидация формы в control__form
+$('.control__form').validate({
+  errorClass: "invalid",
+  rules: {
+    // simple rule, converted to {required:true}
+    userName: {
+      required: true,
+      minlength: 2
+    },
+    userPhone: {
+      required: true,
+      minlength: 17
+    },
+  }, // сообщение 
+  messages: {
+    userName: {
+      required: "Имя обязательно к заполнению",
+      minlength: "Имя должно быть не короче двух букв"
+    },
+    userPhone: "Телефон обязателен",
+  }
+
+});
+
+  //маска для телефона
+  $('[type=tel]').mask('+7(000) 000-00-00', {placeholder: "+7 (___) ___-__-__"});
+
 
 });
